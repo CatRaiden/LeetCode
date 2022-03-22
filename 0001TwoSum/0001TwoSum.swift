14f30a2e9c071095e36c7a432cfe425ac366f3de
Kevin Hsieh <51357065+CatRaiden@users.noreply.github.com>
2022-03-22 23:09:49 +0800

    Update 0001TwoSum.swift

--- 0001TwoSum/0001TwoSum.swift
+++ 0001TwoSum/0001TwoSum.swift
@@ -1,1 +1,12 @@
-
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+  var dict: [Int: Int] = [:]
+  for i in 0...nums.count {
+    let last = target - nums[i]
+    if let j = dict[last], j != i {
+      return [j,i]
+    } else {
+      dict[nums[i]] = i
+    }
+  }
+  return []
+}
